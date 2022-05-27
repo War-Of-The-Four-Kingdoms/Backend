0195659658390e1ebee08813ae52230f830177dc
PongzZzZ <phopthorn.patcharoen@mail.kmutt.ac.th>
2022-05-27 19:08:08 +0700

    leave lobby

--- server.js
+++ server.js
@@ -1,27 +1,20 @@
 const express = require('express');
 const { message } = require('laravel-mix/src/Log');
-
 const app = express();
-
 const server = require('http').createServer(app);
-
 const io = require('socket.io')(server, {
     cors: { origin: "*" }
 });
-
 let users = [];
 let rooms = [];
 io.on('connection', (socket) => {
-    socket.leave(socket.id);
     socket.on('scts',(data) => {
         console.log(data);
         socket.to(data.code).emit('sctc',{username: users.find(u => u.id === socket.id).username, message: data.message});
     });
-
     socket.on('disconnect', () => {
         users = users.filter(u => u.id !== socket.id );
     });
-
     socket.on('start', (username) => {
         const user = {
             username: username,
@@ -29,7 +22,6 @@ io.on('connection', (socket) => {
         };
         users.push(user);
     });
-
     socket.on('create lobby', (data) => {
         socket.join(data.code);
         const room = {
@@ -42,15 +34,13 @@ io.on('connection', (socket) => {
         room.players.push(users.find(u => u.id === socket.id));
         rooms.push(room);
     });
-
     socket.on('join lobby', (data) => {
         socket.join(data.code);
         rooms.find(r => r.code === data.code).players.push(users.find(u => u.id === socket.id));
     });
-
-    socket.on('leave lobby', (code) => {
-        socket.leave(code);
-        rooms.find(r => r.code === code).players.pop(users.find(u => u.id === socket.id));
+    socket.on('leave lobby', (data) => {
+        socket.leave(data.code);
+        rooms.find(r => r.code === data.code).players.pop(users.find(u => u.id === socket.id));
     });
 
     socket.on('list room', () => {
@@ -61,8 +51,6 @@ io.on('connection', (socket) => {
         socket.emit('set room', rooms.find(room => room.players.includes(users.find(u => u.id === socket.id))));
     });
 });
-
-
 server.listen(3000, () => {
     console.log('Server is running');
 });
